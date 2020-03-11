8fd985478bf4a05048c16c3a899faa07fb93f1f1
Gabrielle <eastongabrielle@gmail.com>
2020-03-11 11:37:44 -0400

    layout update

--- script.js
+++ script.js
@@ -9,51 +9,46 @@ const api_key = '0oFslsB2k74JzxNJf3hsTVVXyYvvcHtBuvjF1c2B';
 const web_url = 'https://api.nasa.gov/planetary/apod?api_key=0oFslsB2k74JzxNJf3hsTVVXyYvvcHtBuvjF1c2B';
 const pod = document.querySelector('.hero');
 const button = document.querySelector('#btn');
-const userInput = document.querySelector('#search');
+
 const planetContainer = document.querySelector('.planet-container');
-const readMore = document.querySelector('#btn-read-more');
-const toggle = document.querySelector('#toggle');
-debugger;
+
 //////////////////////////////////////////
 //////////Get Photo of the day///////////
 /////////////////////////////////////////
 const getPhotoOfTheDay = async () => {
   const response = await axios.get("https://api.nasa.gov/planetary/apod?api_key=0oFslsB2k74JzxNJf3hsTVVXyYvvcHtBuvjF1c2B");
-  //debugger;
-  pod.style.background = `url("${response.data.url}")`
+  // pod.style.background = `url("${response.data.url}")`
   pod.innerHTML =
-    `<div class="text">
+    `<div><img src="${response.data.url}"></div>
+    <div class="text">
     <h1>${response.data.title}</h1>
-    <button id="btn-read-more">Read more</button>
-    <div id="toggle">
-    <p>${response.data.explanation}</p></div>
+    <p>${response.data.explanation}</p>
     </div>
     `
+
 }
 getPhotoOfTheDay();
-////////////////////////////////////////////
-/////////////Toggle Description/////////////
-//////////////////////////////////////////
-/////////issue click function doesn't work////
-readMore.addEventListener('click', () => {
-  document.querySelector('#toggle').style.display = "block";
-
-});
 
 
 ////////////////////////////////////////////
 //////api call to get images////////////////
 ///////////////////////////////////////////
-///////issue - is to reset the input///////
-//////////////////////////////////////////
 button.addEventListener('click', async () => {
+  const userInput = document.querySelector('#search');
   const input = userInput.value;
+  console.log(input);
   const response = await axios.get(`https://images-api.nasa.gov/search?q=${input}&media_type=image`);
+  // debugger;
+  console.log(input)
+  console.log(response)
+  console.log(response.data)
   for (let i = 0; i < 6; i += 1) {
+
     planetContainer.innerHTML += `
     <div class="column">
       <img src=${response.data.collection.items[i].links[0].href}>
+      
       <h4>${response.data.collection.items[i].data[0].title}</h4></div>
      `
   }
-})
+});
